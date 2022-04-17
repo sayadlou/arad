254e546d1d87b2f00669753a3fe5296ece347d9b
saeid sayadlou <s.sayadlou@gmail.com>
2022-04-17 16:59:57 +0430

    some change

--- config/settings/base.py
+++ config/settings/base.py
@@ -213,8 +213,8 @@ TINYMCE_DEFAULT_CONFIG = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'https://aradmohajer.ir/',
-    'http://aradmohajer.ir/',
+    'https://aradmohajer.ir',
+    'http://aradmohajer.ir',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
 ]
