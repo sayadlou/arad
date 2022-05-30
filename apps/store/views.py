068a0b37abdeedf6ad459f6b55edcbcb67ed5832
saeid sayadlou <s.sayadlou@gmail.com>
2022-05-30 20:16:57 +0430

    event new style

--- apps/store/views.py
+++ apps/store/views.py
@@ -210,7 +210,6 @@ class ServiceSlugView(DetailView):
         self.object.save()
 
     def get_context_data(self, **kwargs):
-        self._increase_view_counter()
         data = super().get_context_data(**kwargs)
         event = self.object
         user_id = self.request.user.pk
