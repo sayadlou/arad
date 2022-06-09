2dc22a14a6f589845569712357ad439a1b47d2be
saeid sayadlou <s.sayadlou@gmail.com>
2022-06-09 19:13:51 +0430

    gmail add

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -1,6 +1,28 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-
-
 ROOT_URLCONF = 'config.urls.staging'
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE"),
+        "NAME": os.environ.get("SQL_DATABASE"),
+        "USER": os.environ.get("SQL_USER"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD"),
+        "HOST": os.environ.get("SQL_HOST"),
+        "PORT": os.environ.get("SQL_PORT"),
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'yoorusername@gmail.com'
+EMAIL_HOST_PASSWORD = 'key'  # past the key or password app here
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Arad Mohajer Mahan'
+
+CAPTCHA_TEST_MODE = False
