ad607aca83fb09844583eaeb9dd3f5eef5a56329
saeid sayadlou <s.sayadlou@gmail.com>
2022-06-09 16:13:48 +0430

    Profile page complete

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.validators import UnicodeUsernameValidator
+from uuid import uuid4
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager, AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
-from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager, AbstractUser
-from uuid import uuid4
 
 
 class UserProfileManager(UserManager):
@@ -15,7 +14,6 @@ class UserProfile(AbstractUser):
     objects = UserProfileManager()
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    # address = models.TextField(_('address'), max_length=250, null=True, blank=True)
     mobile = models.CharField(_('mobile'), max_length=20, validators=[
         RegexValidator(regex="^(?=.{11})09", message="شماره موبایل معتبر نیست")
     ])
