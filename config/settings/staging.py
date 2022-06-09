b49cbc209df8a6409bf7155cb8733e9a0bcd36d4
saeid sayadlou <s.sayadlou@gmail.com>
2022-06-09 19:16:20 +0430

    gmail add

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -18,9 +18,9 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'yoorusername@gmail.com'
-EMAIL_HOST_PASSWORD = 'key'  # past the key or password app here
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Arad Mohajer Mahan'
