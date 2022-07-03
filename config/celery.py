5934ce55dda4ef4495ec4a571691d4d3fc151976
saeid sayadlou <s.sayadlou@gmail.com>
2022-07-03 21:48:03 +0430

    celery debug

--- config/celery.py
+++ config/celery.py
@@ -1,9 +1,10 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
 
 celery = Celery('config')
-celery.config_from_object('config.settings.base', namespace='CELERY')
+celery.config_from_object(settings, namespace='CELERY')
 celery.autodiscover_tasks()
