07bf9379ae1a895bf3301ceac4ff394e0aab6cf8
sayadlou <s.sayadlou@gmail.com>
2022-07-07 13:10:58 +0430

    celery setup

--- config/celery.py
+++ config/celery.py
@@ -1,9 +1,4 @@
-import os
-
 from celery import Celery
-from django.conf import settings
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
 
 celery = Celery('config')
 celery.config_from_object('config.settings.celery', namespace='CELERY')
